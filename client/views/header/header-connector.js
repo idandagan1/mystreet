51e04b259510e7dba9b7b0639d5ca6dece1d4591
idandagan1 <idandagan1@gmail.com>
2017-01-13 16:55:00 +0200

    Creating branch

--- client/views/header/header-connector.js
+++ client/views/header/header-connector.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as headerActions from './state/header-action-creators';
+import * as headerActions from 'actions/header-action-creators';
 import Header from './components/header';
 
 const select = state => ({
     user: state.user,
     isAuthenticated: state.app.isAuthenticated,
+    street: state.myStreets,
 });
 
 function HeaderConnector(props) {
