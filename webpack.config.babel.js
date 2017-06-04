1bec83ecb7c5e1fa88c61451234514ef9d5006a5
Stephen Lenrow <stephen.lenrow@gmail.com>
2017-06-04 18:04:25 -0400

    Add new webpack file

---
+++ webpack.config.babel.js
@@ -0,0 +1,74 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+    entry: './client/entry.js',
+
+    output: {
+        filename: 'application.js',
+        path: path.join(__dirname, '/dist'),
+        publicPath: '/',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'MyStreet',
+            filename: 'index.html',
+            template: './client/index.html',
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+    ],
+
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        root: [
+            path.resolve(__dirname, 'client'),
+            path.resolve(__dirname, 'node_modules'),
+        ],
+    },
+
+    resolveLoader: {
+        root: path.resolve(__dirname, 'node_modules'),
+    },
+
+    sassLoader: {
+        includePaths: [
+            path.resolve(__dirname, 'client'),
+            path.resolve(__dirname, 'node_modules'),
+        ],
+    },
+
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+            },
+            {
+                loader: 'style!css!sass',
+                test: /\.scss$/i,
+            },
+            {
+                test: /\.(?:png|svg|ico|jpe?g)$/i,
+                loader: 'file-loader',
+                query: { name: 'images/[name].[ext]' },
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader?attrs[]=video:src',
+            },
+            {
+                test: /\.mp4$/,
+                loader: 'url-loader?limit=10000&mimetype=video/mp4',
+            },
+        ],
+    },
+
+    devtool: 'eval',
+};
