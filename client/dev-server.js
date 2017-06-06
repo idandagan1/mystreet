8ad416c762e7fa594d3dada2dcc5bee7c7273b18
Idan Dagan <idandagan1@gmail.com>
2017-06-06 13:48:40 +0300

    Update dev-server.js

--- client/dev-server.js
+++ client/dev-server.js
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import config from '../webpack.config.babel';
 
-const CLIENT_DEV_PORT = 8000;
+const CLIENT_DEV_PORT = process.env.PORT || 8000;
 
 function startServerCallback(err, result) {
     if (err) console.error(err);
