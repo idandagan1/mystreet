d7e1694c935884f741d368ad1520778017c6d48b
Idan Dagan <idandagan1@gmail.com>
2017-09-19 21:15:09 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,15 +11,12 @@ const MongoStore = connectMongo(session);
 const SERVER_DEV_PORT = 8001;
 const port = process.env.PORT || SERVER_DEV_PORT;
 const app = express();
-const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-mongoose.connect('mongodb://emma:Aa123123@ds157233.mlab.com:57233/mstreet');
-
-// if (env === 'development') {
-//     mongoose.connect('mongodb://localhost/mystreet');
-// } else {
-//     mongoose.connect('mongodb://emma:Aa123123@ds127949.mlab.com:27949/mystreetdb');
-// }
+if (process.env.NODE_ENV === 'production') {
+    mongoose.connect('mongodb://emma:Aa123123@ds127949.mlab.com:27949/mystreetdb');
+} else {
+    mongoose.connect('mongodb://localhost/mystreet');
+}
 
 app.use(session({
     secret: 'keyboard cat',
