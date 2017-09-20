44411cc465bc25627e1c72a1b60bd4a1be8853fa
Idan Dagan <idandagan1@gmail.com>
2017-09-20 12:55:59 +0300

    Update webpack.production.config.js

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,10 +4,6 @@ var baseConfig = require('./webpack.base.config.js');
 
 baseConfig.devtool = 'cheap-module-source-map';
 
-baseConfig.entry = {
-    app: '../server/server.js',
-}
-
 baseConfig.plugins = [
     new webpack.DefinePlugin({
         'process.env': {
