55b75cba31cd8a763c059480e65ebd8161dfd99a
idandagan1 <idandagan1@gmail.com>
2017-09-22 10:40:22 +0300

    Create .env file

---
+++ server/config/env/development.js
@@ -0,0 +1,17 @@
+const ENV = process.env,
+    env = 'development',
+    SERVER_DEV_PORT = 8001,
+    port = ENV.PORT || SERVER_DEV_PORT,
+    clientport = 8000,
+    host = ENV.IP || '127.0.0.1',
+    db = 'mongodb://localhost/mystreet',
+    url = 'http://localhost';
+
+module.exports = {
+    port,
+    clientport,
+    host,
+    db,
+    url,
+    env,
+}
