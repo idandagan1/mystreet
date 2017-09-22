38417c57f50a25d92ff311dbcadaa2a383c366a9
Idan Dagan <idandagan1@gmail.com>
2017-09-22 14:24:36 +0300

    Update production.js

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -1,7 +1,7 @@
 const ENV = process.env,
     env = 'production',
     SERVER_DEV_PORT = 8001,
-    port = process.env.PORT, //ENV.PORT || SERVER_DEV_PORT,
+    port = SERVER_DEV_PORT,
     host = ENV.IP || '127.0.0.1',
     db = 'mongodb://emma:Aa123123@ds143734.mlab.com:43734/mystreet',
     url = 'https://mystreet.herokuapp.com';
