70b0b5257cf3c350d224fa0969bb6379afeb5775
idandagan1 <idandagan1@gmail.com>
2017-10-14 17:28:02 +0300

    Fixed profile name in header

--- client/views/my-streets/components/member.js
+++ client/views/my-streets/components/member.js
@@ -4,7 +4,7 @@ import React, { PropTypes } from 'react';
 export default function Member(props) {
 
     const { member, i, userSelected } = props;
-    const { facebook: { id }, name } = member;
+    const { facebook: { id, first_name, last_name } } = member;
     const picturePath = `https://graph.facebook.com/${id}/picture?type=normal`;
 
     return (
@@ -13,7 +13,7 @@ export default function Member(props) {
                 <a className='n-post-user' id='dropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
                     <img className='n-comment-user-icon' alt='user-icon' src={picturePath} role='img' />
                     <div style={{ height: 8 }} />
-                    <div className='n-street-name col-xs-12'>{name}</div>
+                    <div className='n-street-name col-xs-12'>{`${first_name} ${last_name}`}</div>
                     <span className='caret' />
                 </a>
 
